26fa0d7e62e40fc64d2a0df7634a155291dda503
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2022-12-21 16:34:40 +0000

    validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+     redirect_to books_path
+    else
+     render :new
+    end
   end
 
   def show
@@ -20,13 +23,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @book = Book.find(params[:id])
   end
 
   def update
+    is_matching_login_user
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(book.id)
+    if @book.update(book_params)
+     redirect_to book_path(book.id)
+    else
+     render :edit
+    end
   end
 
   def destroy
@@ -39,4 +47,13 @@ private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+  def is_matching_login_user
+    book.user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(book.user_id != login_user_id)
+     redirect_to users_path
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,18 +10,31 @@ class UsersController < ApplicationController
  end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
 
   def update
+   is_matching_login_user
    @user = User.find(params[:id])
-   @user.update(user_params)
-   redirect_to user_path(@user.id)
+   if @user.update(user_params)
+    redirect_to user_path(@user.id)
+   else
+    render :edit
+   end
   end
 
 private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+     redirect_to users_path
+    end
   end
 
 end
